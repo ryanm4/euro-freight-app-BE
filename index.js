de20b17ac4d2603001a387a9ca27a6aaabb69af2
RyanFonseka <64676752+ryanm4@users.noreply.github.com>
2026-08-13 16:11:46 +0530

    json limit extended

--- index.js
+++ index.js
@@ -21,12 +21,10 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
 // app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+
 
 connection.getConnection((err, connection) => {
     if (err) {
@@ -50,3 +48,7 @@ app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/drivers', driverRoutes);
 app.use('/api/v1/wharf', wharfRoutes);
 app.use('/api/v1/freight_staff', freightStaffRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
